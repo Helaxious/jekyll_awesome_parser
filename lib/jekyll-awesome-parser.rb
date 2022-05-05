1807cd7ebd49182587e294c4871f63c8493a98f3
Helaxious <helaxious@hotmail.com>
2022-05-05 12:22:28 -0300

    Added tests, Rakefile, and the parser with the basic methods

--- lib/jekyll-awesome-parser.rb
+++ lib/jekyll-awesome-parser.rb
@@ -1,5 +1,109 @@
 class JekyllAwesomeParser
+
+  def peek(string, pointer, direction, target, stop=nil)
+    if stop.class == String
+      stop = Array(stop)
+    end
+    if target.class == String
+      target = Array(target)
+    end
+    direction = ({"left" => -1, "right" => 1})[direction]
+    if (0 <= pointer + direction) and (pointer + direction <= string.size - 1)
+      if target.include?(string[pointer + direction])
+        return [true, "match", pointer + direction]
+      else
+        if stop != nil && (stop.include?(string[pointer + direction]))
+          return [false, "stop", pointer + direction]
+        else
+          return [false, "no_match", pointer + direction]
+        end
+      end
+    end
+    return [false, "end_of_string", string.size - 1]
+  end
+
+  def peek_until(string, pointer, direction, target, stop=nil)
+    pointer_direction = ({"left" => -1, "right" => 1})[direction]
+    peek_pointer = pointer
+    while true
+      peek_pointer += pointer_direction
+      result = peek(string, peek_pointer, direction, target, stop)
+      if ["match", "end_of_string", "stop"].include?(result[1])
+        return result
+      end
+    end
+  end
+
+  def peek_until_not(string, pointer, direction, target)
+    pointer_direction = ({"left" => -1, "right" => 1})[direction]
+    peek_pointer = pointer
+    while true
+      peek_pointer += pointer_direction
+      result = peek(string, peek_pointer, direction, target, nil)
+      if result[1] == "no_match"
+        return [true, "match", peek_pointer + pointer_direction]
+      else
+        if result[1] == "end_of_string"
+          return [false, "no_match", peek_pointer + pointer_direction]
+        end
+      end
+    end
+  end
+
+  def peek_after(string, pointer, direction, target, target_after, stop=nil)
+    if stop === nil
+      stop = []
+    end
+    if stop.class == String
+      stop = Array(stop)
+    end
+    if target_after.class == String
+      target_after = Array(target_after)
+    end
+
+    if peek(string, pointer, "right", target)[0] == true
+      second_peek = peek_until_not(string, pointer, direction, target)
+      if second_peek[0] == "no_match"
+        return second_peek
+      else
+        is_stop = stop.include?(string[second_peek[2]])
+        if is_stop
+          return [false, "stop", second_peek[2]]
+        end
+        return [target_after.include?(string[second_peek[2]]), "match", second_peek[2]]
+      end
+    else
+      return [false, "no_match", pointer]
+    end
+  end
+
+  def clean_args(arguments)
+    clean_arguments = {}
+    for (key, value) in Array(arguments.clone())
+      if key.include?("*")
+        key = key[1..-1]
+      else
+        if key.include?("=")
+          key = key[0...key.index("=")]
+        end
+      end
+      clean_arguments[key] = value
+    end
+    return clean_arguments
+  end
+
+  def order_result(arguments, result)
+    return arguments.map{|key|[key, result[key]]}.to_h
+  end
+
+  def validate_developer_arguments(args)
+    error_note = "(This is a developer error, this error should be fixed by the\n" +
+                "developers and not the user, if you're the user, contact the developers!)"
+    if args.class != Array
+      raise TypeError, "Wrong type lol" + "\n" + error_note
+    end
+  end
+
   def parse_arguments(args, input)
-    raise "aaa"
   end
 end
--- test/test_parser.rb
+++ test/test_parser.rb
@@ -35,23 +35,101 @@ class TestParser < Minitest::Test
     end
   end
 
-  def test_basic_positional_arguments_and_star_args
+  def test_peek()
     tests = [
-      {"args":["arg1",], "input": "\"jokes\"",
-      "result": {'arg1': ['jokes',]}, "exception": nil},
-
-      {"args":["arg1", "arg2"], "input": "\"jokes\" \"fun_facts\"",
-      "result": {'arg1': ['jokes',], 'arg2': ['fun_facts',]}, "exception": nil},
-
-      {"args":["*arg1",], "input": "\"jokes\" \"fun_facts\"",
-      "result": {'arg1': ['jokes', 'fun_facts']}, "exception": nil},
-
-      {"args":["arg1", "*arg2"], "input": "\"jokes\" \"fun_facts\"",
-      "result": {'arg1': ['jokes',], 'arg2': ['fun_facts',]}, "exception": nil},
-
-      {"args":["arg1", "*arg2"], "input": "\"jokes\" \"fun_facts\" \"games\"",
-      "result": {'arg1': ['jokes',], 'arg2': ['fun_facts', 'games']}, "exception": nil}
+    [["abcdcba", 2, "right", "d", ""],  [true, "match", 3]],
+    [["abcdcba", 4, "left", "d", ""],   [true, "match", 3]],
+    [["abcdcba", 2, "right", "", "d"],  [false, "stop", 3]],
+    [["abcdcba", 4, "left", "", "d"],   [false, "stop", 3]],
+    [["abcdcba", 2, "right", "c", ""],  [false, "no_match", 3]],
+    [["abcdcba", 4, "left", "c", ""],   [false, "no_match", 3]],
+    [["abcdcba", 10, "right", "c", ""], [false, "end_of_string", 6]],
+    [["abcdcba", -1, "right", "c", ""], [false, "no_match", 0]],
+    [["abcdcba", 10, "left", "c", ""],  [false, "end_of_string", 6]],
+    [["abcdcba", 2, "right", ["c", "d", "a"], ""],     [true, "match", 3]],
+    [["abcdcba", 2, "right", "", ["c", "d", "a"], ""], [false, "stop", 3]]
     ]
-    _test(tests)
+    for (input, result) in tests
+      string, pointer, direction, target, stop = input
+      assert_equal(@@parser.peek(string, pointer, direction, target, stop), result)
+    end
   end
+
+  def test_peek_until()
+    tests = [
+    [["abcdcba", 0, "right", "d", ""], [true, "match", 3]],
+    [["abcdcba", 0, "right", "", ""], [false, "end_of_string", 6]],
+    [["abcdcba", 0, "right", "", "d"], [false, "stop", 3]],
+    [["abcdcba", 6, "right", "", "d"], [false, "end_of_string", 6]],
+    [["abcdcba", 6, "left", "", "d"], [false, "stop", 3]],
+    [["abcdcba", 6, "left", "d", ""], [true, "match", 3]],
+    [["abc  d  cba", 10, "left", "d", ""], [true, "match", 5]],
+    [["abc  d  cba", 10, "left", "d", " "], [false, "stop", 7]]
+    ]
+    for (input, result) in tests
+      string, pointer, direction, target, stop = input
+      self.assert_equal(@@parser.peek_until(*input), result)
+    end
+  end
+
+  def test_peek_until_not()
+    tests = [
+    [["       potato", 0, "right", " "], [true, "match", 7]],
+    [[" a      ", 1, "right", " "], [false, "no_match", 8]],
+    [[" a      ", 1, "left", " "], [false, "no_match", -1]],
+    [[" a      ", 5, "left", " "], [true, "match", 1]],
+    [[" a     a ", 5, "right", " "], [true, "match", 7]],
+    [[" aaaaaaa ", 5, "right", "a"], [true, "match", 8]],
+    [[" aaaaaaa ", 5, "left", "a"], [true, "match", 0]],
+    ]
+    for (input, result) in tests
+      string, pointer, direction, target, stop = input
+      assert_equal(@@parser.peek_until_not(*input), result)
+    end
+  end
+
+  def test_peek_after()
+    tests = [
+    [[" aaaaaaa ", 0, "right", " ", "a"],  [false, "no_match", 0]],
+    [["  aaaaaaa ", 0, "right", " ", "a"], [true, "match", 2]],
+    [["aaaaaaa ", 0, "right", "a", " "],   [true, "match", 7]],
+    [["  aaaaaaa", 5, "left", "a", " "],   [true, "match", 1]],
+    [["aa     ", 5, "left", " ", "a"],     [true, "match", 1]]
+    ]
+    for (input, result) in tests
+      string, pointer, direction, target, target_after = input
+      assert_equal(@@parser.peek_after(string, pointer, direction, target, target_after), result)
+    end
+  end
+
+  def test_clean_args()
+    tests = [
+    [{"arg1" => 1, "arg2" => 3}, {"arg1" => 1, "arg2" => 3}],
+    [{"arg1=None" => 1, "*arg2" => 3}, {"arg1" => 1, "arg2" => 3}]
+    ]
+    for (input, result) in tests
+      assert_equal(@@parser.clean_args(input), result)
+    end
+  end
+
+  def test_basic_positional_arguments_and_star_args()
+    skip "I don't even converted the parser yet lol"
+    tests = [
+      {"args" => ["arg1"], "input" => "\"jokes\"",
+      "result" => {"arg1" => ["jokes"]}, "exception" => nil},
+
+      {"args" => ["arg1", "arg2"], "input" => "\"jokes\" \"fun_facts\"",
+      "result" => {"arg1" => ["jokes"], "arg2" => ["fun_facts"]}, "exception" => nil},
+
+      {"args" => ["*arg1"], "input" => "\"jokes\" \"fun_facts\"",
+      "result" => {"arg1" => ["jokes", "fun_facts"]}, "exception" => nil},
+
+      {"args" => ["arg1", "*arg2"], "input" => "\"jokes\" \"fun_facts\"",
+      "result" => {"arg1" => ["jokes"], "arg2" => ["fun_facts"]}, "exception" => nil},
+
+      {"args" => ["arg1", "*arg2"], "input" => "\"jokes\" \"fun_facts\" \"games\"",
+      "result" => {"arg1" => ["jokes"], "arg2" => ["fun_facts", "games"]}, "exception" => nil}]
+    _test(tests, "test_basic_positional_arguments_and_star_args")
+  end
+
 end
