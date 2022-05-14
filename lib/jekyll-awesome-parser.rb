14773924429fd70be5fd0fff752824fc92b1f730
Helaxious <helaxious@hotmail.com>
2022-05-14 15:37:39 -0300

    Also separated method for checking lists

--- lib/jekyll-awesome-parser.rb
+++ lib/jekyll-awesome-parser.rb
@@ -1,4 +1,5 @@
 require_relative "jekyll-awesome-parser/after_parsing.rb"
+require_relative "jekyll-awesome-parser/check_lists.rb"
 require_relative "jekyll-awesome-parser/init_variables.rb"
 require_relative "jekyll-awesome-parser/keyword_arguments.rb"
 require_relative "jekyll-awesome-parser/parser_errors.rb"
@@ -47,48 +48,7 @@ class JekyllAwesomeParser
         next # Don't run the code below, and go to the next iteration
       end
 
-      # To identify the end of a list, even with nested list, we just need to count the number
-      # of opening and closing brackets, when they finally are equal, the list has closed
-      if ["[", "]"].include?(letter) and @flags["matching"] == nil
-        if letter == "]"
-          raise_parser_error(pointer, "ListNotClosedError")
-        else
-          @flags["matching"] = "list"
-          @brackets_count["["] = 1
-          next
-        end
-      end
-
-      if @flags["matching"] == "list"
-        if ["[", "]"].include?(letter)
-          @brackets_count[letter] += 1
-        end
-        if @brackets_count["["] == @brackets_count["]"]
-          # Yeah, creating additional copies of the parser isn't the best idea, I know
-          tmp_parser = JekyllAwesomeParser.new
-
-          # Because the parser doesn't know it's parsing a list, and doesn't know the
-          # specified type, we need to tell it manually
-          full_arg = @clean_lookup[@current_arg] || @current_arg
-          type_name = @type_lookup[full_arg] || @type_lookup[@current_arg]
-
-          tmp_parser.instance_variable_set(:@matching_list, true)
-          tmp_parser.instance_variable_set(:@actual_type_name, type_name)
-
-          parsed_list = tmp_parser.parse_arguments(["*list_arguments"], @tmp_string)
-
-          @current_arg = @clean_lookup[@current_arg] if @clean_lookup.include?(@current_arg)
-          @parsed_result[@current_arg] += [parsed_list["list_arguments"]]
-          bump_current_arg(pointer, letter)
-
-          @brackets_count = {"[" => 0, "]" => 0}
-          @flags["matching"] = nil
-          @tmp_string = ""
-        else
-          @tmp_string += letter
-        end
-        next
-      end
+      next if check_lists(pointer, letter) == "next"
 
       if @flags["matching"] == "argument"
         check_quoteless_strings(pointer, letter)
@@ -98,8 +58,8 @@ class JekyllAwesomeParser
           next
         end
 
+        # Ignore if the escape character is not being escaped
         if letter == "\\"
-          # Ignore if the escape character is not being escaped
           next if peek(input, pointer, "left", "\\")[1] != "match"
         end
         @tmp_string += letter
---
+++ lib/jekyll-awesome-parser/check_lists.rb
@@ -0,0 +1,48 @@
+class JekyllAwesomeParser
+  # Recursively calls parse_argument in the list match to parse inside of it
+  def parse_list(pointer, letter)
+    # Yeah, creating additional copies of the parser isn't the best idea, I know
+    tmp_parser = JekyllAwesomeParser.new
+
+    # Because the parser doesn't know it's parsing a list, and doesn't know the
+    # specified type, we need to tell it manually
+    full_arg = @clean_lookup[@current_arg] || @current_arg
+    type_name = @type_lookup[full_arg] || @type_lookup[@current_arg]
+
+    tmp_parser.instance_variable_set(:@matching_list, true)
+    tmp_parser.instance_variable_set(:@actual_type_name, type_name)
+
+    parsed_list = tmp_parser.parse_arguments(["*list_arguments"], @tmp_string)
+
+    @current_arg = @clean_lookup[@current_arg] if @clean_lookup.include?(@current_arg)
+    @parsed_result[@current_arg] += [parsed_list["list_arguments"]]
+    bump_current_arg(pointer, letter)
+
+    @brackets_count = {"[" => 0, "]" => 0}
+    @flags["matching"] = nil
+    @tmp_string = ""
+  end
+
+  def check_lists(pointer, letter)
+    # To identify the end of a list, even with nested list, we just need to count the number
+    # of opening and closing brackets, when they finally are equal, the list has closed
+    if ["[", "]"].include?(letter) and @flags["matching"] == nil
+      raise_parser_error(pointer, "ListNotClosedError") if letter == "]"
+
+      @flags["matching"] = "list"
+      @brackets_count["["] = 1
+      return "next"
+    end
+
+    if @flags["matching"] == "list"
+      @brackets_count[letter] += 1 if ["[", "]"].include?(letter)
+      # If the list was closed
+      if @brackets_count["["] == @brackets_count["]"]
+        parse_list(pointer, letter)
+      else
+        @tmp_string += letter
+      end
+      return "next"
+    end
+  end
+end
